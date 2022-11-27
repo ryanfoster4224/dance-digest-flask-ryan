3a8363a915598d1ad2282ae208b0ce867ba50880
vetsinen <vetsinen@gmail.com>
2022-11-27 20:43:02 +0200

    mysql seems to work

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, jsonify
 import os
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()  # take environment variables from .env.
+
+from flaskext.mysql import MySQL
+mysql = MySQL()
 
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('MYSQL_DATABASE_PORT'))
 
+mysql.init_app(app)
+
+conn = mysql.connect()
 
 @app.route('/')
 def index():
