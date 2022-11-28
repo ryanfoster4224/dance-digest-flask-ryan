6a5dc0706a3b718164b4766a572f046bc64569fe
vetsinen <vetsinen@gmail.com>
2022-11-28 11:32:04 +0200

    selects data as json

--- main.py
+++ main.py
@@ -14,12 +14,20 @@ app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('MYSQL_DATABASE_PORT'))
 
 mysql.init_app(app)
-
 conn = mysql.connect()
+cursor = conn.cursor()
+
+rez = cursor.execute('SELECT * FROM test2')
+data = cursor.fetchall()
+
+print(data)
+cursor.close()
+
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to dance-digest Flask"})
+    return jsonify(data)
+    # return jsonify({"Choo Choo": "Welcome to dance-digest Flask"})
 
 
 if __name__ == '__main__':
