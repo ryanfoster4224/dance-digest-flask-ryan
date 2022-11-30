faeda27a08d7d19ff730cf974d47ca9863e34cf8
vetsinen <vetsinen@gmail.com>
2022-11-30 12:43:12 +0200

    login implemented

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-from flask import Flask, jsonify
-import os
-from dotenv import load_dotenv, dotenv_values
+from flask import Flask, jsonify, request
+import os, hashlib
+from dotenv import load_dotenv
+
 load_dotenv()  # take environment variables from .env.
 
 from flaskext.mysql import MySQL
-mysql = MySQL()
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__, static_url_path='/static')
 
+mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
 app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
 app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_PORT'] = int(os.getenv('MYSQL_DATABASE_PORT'))
-
 mysql.init_app(app)
 conn = mysql.connect()
-cursor = conn.cursor()
 
-rez = cursor.execute('SELECT * FROM test2')
-data = cursor.fetchall()
-
-print(data)
-cursor.close()
 
 @app.route('/')
 def index():
@@ -30,9 +25,58 @@ def index():
 
 
 @app.route('/api/events')
-def events():
+def events_list():
+    cursor = conn.cursor(DictCursor)
+
+    rez = cursor.execute('SELECT * FROM parties')
+    data = cursor.fetchall()
+    print(data)
+    cursor.close()
+
     return jsonify(data)
-    # return jsonify({"Choo Choo": "Welcome to dance-digest Flask"})
+
+
+@app.route('/api/event/', methods=['POST'])
+def event_add():
+    print('adding ')
+    return jsonify('maybe')
+
+
+@app.route('/api/event/<int:id>', methods=['DELETE'])
+def event_delete(id: int):
+    print('deleting ' + str(id))
+    return jsonify('maybe')
+
+
+@app.route('/api/user/')
+def get_permissions():
+    username = request.args.get('username', type=str)
+    password = request.args.get('password', type=str)
+
+    if len(username) < 3 or len(password) < 3:
+        return jsonify({'authorId': -1, 'superAdmin': False})
+    hash = ("SHA-256:", hashlib.sha256(password.encode()).hexdigest())[1]
+
+    cursor = conn.cursor(DictCursor)
+    rez = cursor.execute(f'SELECT authorId, hash, superAdmin FROM users WHERE username="{username}"')  # TODO: sanitize
+    data = cursor.fetchone()
+
+    if not data:
+        return jsonify({'authorId': -1, 'superAdmin': False})
+
+    print(data)
+    if data['hash'] == hash:
+        return jsonify({'authorId': data['authorId'], 'superAdmin': bool(data['superAdmin'])})
+
+    cursor.close()
+
+    print(username, hash)
+    return jsonify({'authorId': 100, 'superAdmin': False})
+
+
+@app.route('/api/user', methods=['POST'])
+def user_create():
+    pass
 
 
 if __name__ == '__main__':
---
+++ passandhashgen.py
@@ -0,0 +1,20 @@
+import hashlib
+
+import uuid
+
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4())  # Convert UUID format to a Python string.
+    random = random.upper()  # Make all characters uppercase.
+    random = random.replace("-", "")  # Remove the UUID '-'.
+    return random[0:string_length]  # Return the random string.
+
+
+pw = '4yFG5byD88'
+pw = my_random_string(10)
+print(pw)
+# encode it to bytes using UTF-8 encoding
+message = b"Some text to hash"
+hash = ("SHA-256:", hashlib.sha256(pw.encode()).hexdigest())
+print(hash[1])
