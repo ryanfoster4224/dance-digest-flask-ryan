d7b081a8d93cc5ba6444239b951476862edd7dd6
vetsinen <vetsinen@gmail.com>
2022-12-07 02:52:51 +0200

    saves event to mongo

--- main.py
+++ main.py
@@ -4,33 +4,32 @@ from dotenv import load_dotenv
 from flask_mongoengine import MongoEngine
 from mongoengine import *
 import hashlib
-
+from bson.objectid import ObjectId
 
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__, static_url_path='/static')
-# app.config['MONGODB_SETTINGS'] =  {
-#     "DB": "main",
-#     "host":'mongodb+srv://superadmin:rbDkYE2K4Qir69Om@dvas-cluster0.o1qow.mongodb.net/?retryWrites=true&w=majority',
-#     "alias":"default"
-# }
 
 connect(db = "main", host = 'mongodb+srv://superadmin:rbDkYE2K4Qir69Om@dvas-cluster0.o1qow.mongodb.net/?retryWrites=true&w=majority',
     alias = "default")
 
 class Event(Document):
     title = StringField(max_length=50, required=True)
-    price = StringField(max_length=12)
+    price = StringField(max_length=12, default='500')
+    authorId = StringField(default="")
+    full = StringField()
+    brief = StringField()
+    balance = StringField(default="merengue")
+    location = StringField(default='АТБ на Виноградарі')
+    start = StringField(max_length=5, default='07:00')
+    weekdayId = IntField()
+    weekday = StringField()
+    published = BooleanField(default=False)
 
     meta = {"collection":"events"}
 
-class InputUser(Document):
-    username = StringField(min_length=3, required=True)
-    password = StringField(required=True)
-    superAdmin = BooleanField(default=False)
-
 class User(Document):
-    username = StringField(min_length=3, required=True)
+    username = StringField(min_length=3, unique=True, required=True)
     hash = StringField(required=True)
     superAdmin = BooleanField(default=False)
 
@@ -43,13 +42,76 @@ def events_list():
 def event_add():
     body = request.json
     print(body)
-    event = Event(title = body['title'], price = body['price'])
+    event = Event(title = body['title'],
+                  price = body['price'],
+                  authorId = body['authorId'],
+                  full = body['full'],
+                  brief = body.get('brief', None),
+                  published = body['published'],
+                  balance = body['balance'],
+                  weekdayId = body.get('weekdayId',-1),
+                  weekday = body.get('weekday',None),
+                  start = body.gey('start', None),
+                  location = body.get('location', None)
+                  )
     event.save()
-    return {"hello":"world"}
+    return {"status":"added"}
+
+@app.route('/api/event', methods = ['PUT'])
+def event_update():
+    body = request.json
+    print(body)
+    try:
+        Event.objects(pk=body['id'])\
+            .update(title=body['title'], price=body['price'])
+    except:
+        return jsonify({"status":"error on update"})
+
+    return jsonify({"status":"updated"})
+
+@app.route('/api/event/<string:sid>', methods=['DELETE'])
+def event_delete(sid: str):
+    print('deleting ' + (sid))
+
+    try:
+        de = Event.objects(pk=sid).first()
+        print(type(de), de.id)
+        de.delete()
+    except:
+        print('deleting error')
+        return jsonify({"status":"deleting error"})
+
+    return jsonify({"status":"deleted sucessfully"})
+
+@app.route('/api/user/')
+def get_permissions():
+    username = request.args.get('username', type=str)
+    password = request.args.get('password', type=str)
+
+    if len(username) < 3 or len(password) < 3:
+        return jsonify({'authorId': "", 'superAdmin': False})
+    hash = ("SHA-256:", hashlib.sha256(password.encode()).hexdigest())[1]
+    try:
+        user = User.objects(username=username).first()
+    except:
+        return jsonify({'authorId': "", 'superAdmin': False})
+
+    if user:
+        print(type(user))
+        print(type(user.pk), type(user.superAdmin))
+        print(user.pk, user.superAdmin)
+        if user.hash == hash:
+            # return jsonify("maybe")
+            return jsonify({'authorId':str(user.pk),'superAdmin':bool(user.superAdmin)})
+
+    else:
+        return jsonify({'authorId': -1, 'superAdmin': False})
 
 @app.route('/api/user', methods = ['POST'])
 def user_add():
     body = request.json
+    if len(body['password'])<3:
+        return "too short password"
     # encode it to bytes using UTF-8 encoding
     body['hash'] = ("SHA-256:", hashlib.sha256(body['password'].encode()).hexdigest())[1]
     # body['hash'] = 'fsdfsdf'
@@ -71,5 +133,7 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
+    #user = User.objects(username='cherk').first()
+
     app.run(debug=True, port=os.getenv("PORT", default=5000))
 
